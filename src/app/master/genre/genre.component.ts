3a6324ba5de7cf5d738aedb4edf38daaaeb500b6
hariprasadsh <63358097+hariprasadsh@users.noreply.github.com>
2020-05-24 02:49:08 +0530

    added a demo grid with hardcoded data

--- src/app/master/genre/genre.component.ts
+++ src/app/master/genre/genre.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-genre',
   templateUrl: './genre.component.html',
-  styleUrls: ['./genre.component.scss']
+  styleUrls: ['./genre.component.scss'],
 })
 export class GenreComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  columnDefs = [
+    { headerName: 'Name', field: 'genreName', sortable: true },
+    { headerName: 'Tag', field: 'genreTag', sortable: true },
+  ];
 
+  rowData = [
+    { genreName: 'Drama', genreTag: 'drama' },
+    { genreName: 'Comedy', genreTag: 'comedy' },
+    { genreName: 'Action', genreTag: 'action' },
+    { genreName: 'Thriller', genreTag: 'thriller' },
+    { genreName: 'Sci-fi', genreTag: 'scifi' },
+  ];
 }
--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenreComponent } from './genre/genre.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [GenreComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AgGridModule.withComponents([])],
 })
 export class MasterModule {}
